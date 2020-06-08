0777560d224bfd3a8d7fbb892b3a96c50ff45dcf
Oliver Comprés Poulsen <oli@live.dk>
2020-06-08 14:27:43 +0200

    First commit

---
+++ src/main/java/CDIO_Final/App.java
@@ -0,0 +1,13 @@
+package CDIO_Final;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
