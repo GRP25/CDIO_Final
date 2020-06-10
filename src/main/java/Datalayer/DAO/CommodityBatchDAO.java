6485c3d55046ff0f31d3d5ba88727b023f9448b5
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-06-10 23:16:23 +0200

    Implement CommodityBatchDAO

--- src/main/java/Datalayer/DAO/CommodityBatchDAO.java
+++ src/main/java/Datalayer/DAO/CommodityBatchDAO.java
@@ -1,42 +1,104 @@
 package Datalayer.DAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 
-import Datalayer.DTO.commodityBatchDTO;
+import DB.DBUtil;
+import Datalayer.DTO.CommodityBatchDTO;
 import Datalayer.Interfaces.ICommodityBatchDAO;
 
 @RequestScoped
 public class CommodityBatchDAO implements ICommodityBatchDAO {
 
 	@Override
-	public commodityBatchDTO getCommodityBatch(int commodityBatch_id) {
-		// TODO Auto-generated method stub
-		return null;
+	public CommodityBatchDTO getCommodityBatch(int commodityBatch_id) throws DALException {
+
+		String query = "SELECT * FORM CommodityBatch WHERE commodityBatchId = ?";
+
+			ResultSet resultSet = DBUtil.executeSelectQuery( query, DBUtil.convertTOObject( commodityBatch_id ) );
+
+            CommodityBatchDTO commodityBatchDTO;
+
+				try {
+					resultSet.first();
+                    commodityBatchDTO = (CommodityBatchDTO) DBUtil.resultSetToObject( resultSet,  CommodityBatchDTO.class);
+				} catch (SQLException e) {
+					e.printStackTrace();
+					return null;
+				}
+
+
+		return commodityBatchDTO;
 	}
 
 	@Override
-	public ArrayList<commodityBatchDTO> getCommodityBatchList() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<CommodityBatchDTO> getCommodityBatchList() throws DALException {
+		String query = "SELECT * FROM CommodityBatch";
+
+		    ResultSet resultSet = DBUtil.executeSelectQuery( query, null );
+		    List<CommodityBatchDTO> listOfCommodityBatchDTO;
+		    try {
+		        CommodityBatchDTO commodityBatchDTO;
+		        listOfCommodityBatchDTO = new ArrayList<>();
+		        while (resultSet.next()) {
+		            commodityBatchDTO = (CommodityBatchDTO) DBUtil.resultSetToObject( resultSet, CommodityBatchDTO.class );
+		            listOfCommodityBatchDTO.add(commodityBatchDTO);
+                }
+            } catch (SQLException e) {
+		        e.printStackTrace();
+		        return null;
+            }
+
+		return listOfCommodityBatchDTO;
 	}
 
 	@Override
-	public ArrayList<commodityBatchDTO> getCommodityBatchList(int commodityBatch_id) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<CommodityBatchDTO> getCommodityBatchList(int commodityBatch_id) throws DALException {
+		String query = "SELECT * FROM CommodityBatch";
+
+		    ResultSet resultSet = DBUtil.executeSelectQuery( query, DBUtil.convertTOObject( commodityBatch_id ) );
+		    List<CommodityBatchDTO> listOfCommodityBatchDTO = null; // Instead of implementing same as getCommodityBatchList out parameter
+		    try {
+		        CommodityBatchDTO commodityBatchDTO;
+		        listOfCommodityBatchDTO = new ArrayList<>();
+		        while (resultSet.next()) {
+		            commodityBatchDTO = (CommodityBatchDTO) DBUtil.resultSetToObject( resultSet, CommodityBatchDTO.class );
+		            listOfCommodityBatchDTO.add( commodityBatchDTO );
+                }
+            } catch (SQLException e) {
+		        e.printStackTrace();
+            }
+
+		return listOfCommodityBatchDTO;
 	}
 
 	@Override
-	public void createCommodityBatch(commodityBatchDTO batch) {
-		// TODO Auto-generated method stub
+	public void createCommodityBatch(CommodityBatchDTO batch) throws DALException {
+		String query = "INSERT INTO CommodityBatch (commodityBatchId, commodityId, weight, supplier) VALUES (?, ?, ?, ?)";
+		Object[] parameter = DBUtil.convertTOObject( batch );
+
+            try {
+                DBUtil.executeCreateAndUpdate( query, parameter );
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
 
 	}
 
 	@Override
-	public void updateCommodityBatch(commodityBatchDTO batch) {
-		// TODO Auto-generated method stub
+	public void updateCommodityBatch(CommodityBatchDTO batch) throws DALException {
+		String query = "UPDATE CommodityBatch SET commodityId=?, weight=?, supplier=?) WHERE commodityBatchId=?";
+
+		    try {
+		        DBUtil.executeCreateAndUpdate( query, DBUtil.convertTOObject( batch ) );
+            } catch (SQLException e) {
+		        e.printStackTrace();
+            }
 
 	}
 }
