f637612f2db383cf2e2f356521636714e4b6e847
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-06-10 18:32:38 +0200

    Creat class exception and add throws DALException to ICommodityBatchDAO interface

--- src/main/java/Datalayer/Interfaces/ICommodityBatchDAO.java
+++ src/main/java/Datalayer/Interfaces/ICommodityBatchDAO.java
@@ -2,18 +2,19 @@ package Datalayer.Interfaces;
 
 import java.util.ArrayList;
 
+import Datalayer.DAO.DALException;
 import Datalayer.DTO.commodityBatchDTO;
 
 public interface ICommodityBatchDAO {
 
-	commodityBatchDTO getCommodityBatch(int commodityBatch_id);
+	commodityBatchDTO getCommodityBatch(int commodityBatch_id) throws DALException;
 
-	ArrayList<commodityBatchDTO> getCommodityBatchList();
+	ArrayList<commodityBatchDTO> getCommodityBatchList() throws DALException;
 
-	ArrayList<commodityBatchDTO> getCommodityBatchList(int commodityBatch_id);
+	ArrayList<commodityBatchDTO> getCommodityBatchList(int commodityBatch_id) throws DALException;
 
-	void createCommodityBatch(commodityBatchDTO batch);
+	void createCommodityBatch(commodityBatchDTO batch) throws DALException;
 
-	void updateCommodityBatch(commodityBatchDTO batch);
+	void updateCommodityBatch(commodityBatchDTO batch) throws DALException;
 
 }
