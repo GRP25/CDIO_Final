609ce986d0f860f9356901ad7613f5d99ad24135
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-06-11 15:20:23 +0200

    Implement CommodityBatchResource class

--- src/main/java/REST/Resources/CommodityBatchResource.java
+++ src/main/java/REST/Resources/CommodityBatchResource.java
@@ -1,6 +1,5 @@
 package REST.Resources;
 
-import Datalayer.DAO.DALException;
 import Datalayer.DTO.CommodityBatchDTO;
 import REST.Services.Implementation.CommodityBatchService;
 import REST.Services.Interfaces.ICommodityBatchService;
@@ -12,29 +11,65 @@ import javax.ws.rs.core.Response;
 
 import java.util.List;
 
-@Path( "/commodityBatch/" )
+@Path( "commodityBatch" )
 public class CommodityBatchResource {
 
     ICommodityBatchService commodityBatchService = new CommodityBatchService();
 
     @GET
+    @Path("{commodityBatch_id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getCommodityBatchList() throws DALException {
+    public Response getCommodityBatch(@PathParam( "commodityBatch_id" ) int commodityBatch_id){
+
+        Response response;
+        CommodityBatchDTO commodityBatchDTO = commodityBatchService.getCommodityBatch( commodityBatch_id );
+
+        if (commodityBatchDTO != null)
+            response = Response.status( Response.Status.OK ).entity( commodityBatchDTO ).build();
+        else
+            response = Response.status( Response.Status.OK ).entity( "Exception: from CommodityBatchResource.getCommodityBatch" ).build();
+
+        return response;
+
+    }
+
+    @GET
+    @Path( "{commodity_id}" )
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCommodityBatchList(@PathParam( "commodity_id" ) int commodity_id) {
+
+        Response response;
+        List<CommodityBatchDTO> commodityBatchDTOList = commodityBatchService.getCommodityBatchList(commodity_id);
+
+        if (commodityBatchDTOList != null)
+            response = Response.status( Response.Status.OK ).entity( commodityBatchDTOList ).build();
+        else
+            response = Response.status( Response.Status.BAD_REQUEST ).entity( "Exception: from CommodityBatchResource.getCommodityBatchList_withParameter" ).build();
+
+        return response;
+
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCommodityBatchList() {
+
         Response response;
         List<CommodityBatchDTO> commodityBatchDTOList = commodityBatchService.getCommodityBatchList();
 
         if (commodityBatchDTOList != null)
             response = Response.status( Response.Status.OK ).entity( commodityBatchDTOList ).build();
         else
-            response = Response.status( Response.Status.BAD_REQUEST ).entity( "Expstion: from CommodityBatchResource.getCommodityBatchList" ).build();
+            response = Response.status( Response.Status.BAD_REQUEST ).entity( "Exception: from CommodityBatchResource.getCommodityBatchList" ).build();
 
         return response;
+
     }
 
     @PUT
-    @Consumes( MediaType.APPLICATION_JSON )
-    @Produces ( MediaType.APPLICATION_JSON )
-    public Response createCommodityBatch(CommodityBatchDTO commodityBatchDTO) throws DALException {
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createCommodityBatch(CommodityBatchDTO commodityBatchDTO) {
 
         Response response;
         String createResult = commodityBatchService.createCommodityBatch( commodityBatchDTO );
@@ -45,6 +80,25 @@ public class CommodityBatchResource {
             response = Response.status( Response.Status.BAD_REQUEST ).entity( createResult ).build();
 
         return response;
+
+    }
+
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateCommodityBatch(CommodityBatchDTO commodityBatchDTO) {
+
+        Response response;
+        String updateResult = commodityBatchService.updateCommodityBatch( commodityBatchDTO );
+
+        if (updateResult.equalsIgnoreCase( "Update query executed successfully" ))
+            response = Response.status( Response.Status.OK ).entity( updateResult ).build();
+        else
+            response = Response.status( Response.Status.BAD_REQUEST ).entity( updateResult ).build();
+
+        return response;
+
     }
 
 }
