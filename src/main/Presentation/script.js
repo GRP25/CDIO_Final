4c870ce3f423f9b4163605966b4f5a3427c4c636
Oliver Comprés Poulsen <oli@live.dk>
2020-06-15 15:08:50 +0200

    show list of users and their content

--- src/main/Presentation/script.js
+++ src/main/Presentation/script.js
@@ -14,8 +14,60 @@ function createUser() {
 
 }
 
-function listUser() {
+
+function createUser() {
     
+}
+
+function listUsers() {
+   // document.getElementById("loaderID").style.display = "block";
+    $.ajax({
+      url: "https://api.mama.sh/userresource",
+      contentType: "application/json",
+      method: "GET",
+      success: function (response) {
+        //document.getElementById("loaderID").style.display = "none";
+        $("#ListOfUsersTable").html("");
+        var html = "";
+          //'<table class="tableOfUsers"> <tr><th>Name</th><th>Id</th></tr>';
+          jQuery.each(response, (i, item) => {
+          html += `<tr>`;
+          html += `<td> ${item.userID}  ${item.firstName} ${item.surname}</td>`;
+          html += `<td><button class="w3-dark-grey list-item-btn"> ${getStatus(item.status)} <i class="fa fa-close"></i></button> <button onclick="getUser(${item.userID});" id="EditBtn" class="w3-dark-grey list-item-btn">Vis <i class="fa fa-cog fa-fw"></i></button></td>`;
+          html += `</tr>`;
+        });
+        console.log(html);
+        $("#ListOfUsersTable").append(html);
+  
+        $("#ListOfUsersTable").show();
+      },
+      error: function (jqXHR, text, error) {
+        document.getElementById("loaderID").style.display = "none";
+        alert(jqXHR.status + text + error);
+      },
+    });
+  
+   // $("#list").show();  
+}
+
+function getUser(id) {
+    $.ajax({
+        url: "https://api.mama.sh/userresource/" + id,
+        contentType: "application/json",
+        method: "GET",
+        success: function (response) {
+          document.getElementById("EditUserWindow").style.display = "block";
+          $("#UserIDTxt").val(response.userID);
+          $("#UserFirstNameTxt").val(response.firstName);
+          $("#UserLastName").val(response.surname);
+          $("#UserIniTxt").val(response.cpr);
+          console.log("getuser Started");
+        },
+        error: function (jqXHR, text, error) {
+          document.getElementById("loaderID").style.display = "none";
+          alert(jqXHR.status + text + error);
+        },
+      });
 }
 
 function printUserToTable(id, name, status) {
@@ -24,7 +76,7 @@ function printUserToTable(id, name, status) {
 }
 
 function getStatus(status) {
-    if (status == 1) {
+    if (status == 0) {
         return "aktiver";
     }
     else {
