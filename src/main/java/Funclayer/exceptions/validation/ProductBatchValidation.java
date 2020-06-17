ca36fe3a0af308f8b947bcc85110f9a1d1bc88fc
flere <s147199@student.dtu.dk>
2020-06-17 16:33:09 +0200

    ProductBatchValidation etc

--- src/main/java/Funclayer/exceptions/validation/ProductBatchValidation.java
+++ src/main/java/Funclayer/exceptions/validation/ProductBatchValidation.java
@@ -4,10 +4,14 @@ import Datalayer.DAO.ProductBatchDAO;
 import Datalayer.DTO.ProductBatchDTO;
 import Datalayer.Interfaces.IProductBatchDAO;
 import Funclayer.exceptions.exceptions.DataLayerException;
+import Funclayer.exceptions.exceptions.NotAStatusException;
 import Funclayer.exceptions.exceptions.NotProductBatchExeption;
 import Funclayer.exceptions.validation.template.Validation;
 
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
 
 public class ProductBatchValidation extends Validation {
 
@@ -19,9 +23,26 @@ public class ProductBatchValidation extends Validation {
     }
 
     public static void productBatchValidation(ProductBatchDTO productBatchDTO) throws NotProductBatchExeption, SQLException {
-        isDateValidator(productBatchDTO.getStartDate());
-        isDateValidator(productBatchDTO.getEndDate());
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String tempStartDate = dateFormat.format(productBatchDTO.getStartDate());
+        String tempEndDate = dateFormat.format(productBatchDTO.getEndDate());
+
+        if (!isDateValidator(tempStartDate))
+            throw new DateTimeException("Invalid start-date");
+
+        if (!isDateValidator(tempEndDate))
+            throw new DateTimeException("Invalid end-date");
+
         statusValidator(productBatchDTO.getStatus());
+
         validateProductBatchID(productBatchDTO.getProductBatch_id());
+
+
+
     }
+
+
+
+
 }
