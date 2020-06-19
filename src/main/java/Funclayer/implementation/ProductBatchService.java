4ce3edca09f811a3f385682c496da36d876d9990
Thomas <thomas8@hohnen.dk>
2020-06-19 16:17:19 +0200

    something

--- src/main/java/Funclayer/implementation/ProductBatchService.java
+++ src/main/java/Funclayer/implementation/ProductBatchService.java
@@ -8,6 +8,7 @@ import Funclayer.interfaces.IProductBatchService;
 import java.sql.SQLException;
 import java.util.List;
 import Funclayer.exceptions.validation.ProductBatchValidation;
+
 public class ProductBatchService implements IProductBatchService {
 
     IProductBatchDAO productBatchDAO = new ProductBatchDAO();
@@ -33,7 +34,7 @@ public class ProductBatchService implements IProductBatchService {
 
     @Override
     public String createProductBatch(ProductBatchDTO productBatch) throws SQLException, NotProductBatchExeption {
-        productBatchValidationForCreate(productBatch);
+        productBatchValidatoinForCreate(productBatch);
         productBatchDAO.createProductBatch(productBatch);
         return "Insert query executed successfully";
     }
