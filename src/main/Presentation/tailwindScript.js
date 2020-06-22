e788616d357225e542336fa4fc15416333595558
Daniel <s143861@student.dtu.dk>
2020-06-22 09:23:35 +0200

    Missing some functions

--- src/main/Presentation/tailwindScript.js
+++ src/main/Presentation/tailwindScript.js
@@ -1,12 +1,200 @@
 $(document).ready(() => {
+	$('.valgmuligheder').on('click', function () {
+		var target = $(this).data('target');
+		$('.mainScreen').not(target).hide();
+		$(target).show();
+	})
+});
+
+function listCommodities() {
+	event.preventDefault();
+	console.log("test");
+	// document.getElementById("loaderID").style.display = "block";
+	$.ajax({
+		url: "https://api.mama.sh/commodity",
+		contentType: "application/json",
+		method: "GET",
+		success: function (response) {
+			console.log("test");
+			//document.getElementById("loaderID").style.display = "none";
+			$("#listOfCommoditiestable").html("");
+			let html = `<thead><tr><th class="w-1/2 px-4 py-2">Råvare navn</th><th class="w-1/2 px-4 py-2">Råvare id</th></tr></thead><tbody>`
+			//'<table class="tableOfUsers"> <tr><th>Name</th><th>Id</th></tr>';
+			jQuery.each(response, (i, item) => {
+				html += `<tr>`;
+				html += `<td class="border px-4 py-2"> <span class="float-left"> ${item.commodity_Name}</td>`;
+				html += `<td class="border px-4 py-2">
+					<span class="commodity-id float-left"> ${item.commodity_id}</span>
+					<div class="flex inline-block justify-end px-2 ">
+								<button
+									class="modal-open bg-purple-600 hover:bg-purple-800 text-white font-bold px-2 py-1 mx-2 rounded float-right" onClick="getCommodity(${item.commodity_id})">vis</button>
+								<button
+									class="bg-purple-600 hover:bg-purple-800 text-white font-bold px-2 py-1 mx-2 rounded float-right">slet</button>
+							</div>
+				</td>`;
+				html += `</tr>`;
+			});
+			html += `</tbody>`
+			console.log(html);
+			$("#listOfCommoditiestable").append(html);
+
+			applyModal();
+
+			$("#listOfCommoditiestable").show();
+		},
+		error: function (jqXHR, text, error) {
+			alert(jqXHR.status + text + error);
+		},
+	})
+};
+
+
+function toggleModal() {
+	const body = document.querySelector('body')
+	const modal = document.querySelector('.modal')
+	modal.classList.toggle('opacity-0')
+	modal.classList.toggle('pointer-events-none')
+	body.classList.toggle('modal-active')
+}
+
+function createCommodityModal(funktion) {
+	toggleModal()
+	$("#modal-body").html("");
+	$("#modal-body").append(`<form>
+			<label>Råvare Navn:</label>
+			<input class="shadow appearance-none border rounded text-gray-700 py-2 px-3" type="text"
+				placeholder="" id="showRåvareNavn"></input> <br />
+
+			<label>Råvare id:</label>
+			<input class="shadow appearance-none border rounded text-gray-700 py-2 px-3" type="text"
+				placeholder="" id="showRåvareId"></input> <br />
+
+		</form>`);
+	$("#modal-footer").html("");
+	$("#modal-footer").append(
+		`<div>
+					<button
+						class="modal-update px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
+						onclick=${funktion}>Opret </button>
+					<button
+						class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
+				</div>`
+	);
+
+
+	applyModal()
+
+
+}
+
+function createPrescriptionModal(funktion) {
+	toggleModal()
+	$("#modal-body").html("");
+	$("#modal-body").append(`<form>
+			<label>Råvare Navn:</label>
+			<input class="shadow appearance-none border rounded text-gray-700 py-2 px-3" type="text"
+				placeholder="" id="showPrescriptionName"></input> <br />
+
+			<label>Råvare id:</label>
+			<input class="shadow appearance-none border rounded text-gray-700 py-2 px-3" type="text"
+				placeholder="" id="showPrescriptionId"></input> <br />
+
+		</form>`);
+	$("#modal-footer").html("");
+	$("#modal-footer").append(
+		`<div>
+					<button
+						class="modal-update px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
+						onclick=${funktion}>Opret </button>
+					<button
+						class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
+				</div>`
+	);
+
+
+}
+
+function createCommodity() {
+	console.log("create commodity started");
+	const commodity = {
+		commodity_Name: $("#showRåvareNavn").val(),
+		commodity_id: $("#showRåvareId").val(),
+	};
+	console.log(commodity.commodity_Name);
+
+	$.ajax({
+		url: "https://api.mama.sh/commodity",
+		contentType: "application/json",
+		method: "POST",
+		data: JSON.stringify(commodity),
+		succes: function (response) {
+			toggleModal()
+			listCommodities()
+		},
+		error: function (jqXHR, text, error) {
+			alert(jqXHR.status + text + error);
+		},
+	})
+}
+
+
+
+function getCommodity(id) {
+	console.log("getuser Started");
+	createCommodityModal("updateCommodity()");
+	$.ajax({
+		url: `https://api.mama.sh/commodity/${id}`,
+		contentType: "application/json",
+		method: "GET",
+		success: function (response) {
+			console.log("Hej");
+			console.log(response.commodity_Name);
+			toggleModal();
+			$("#showRåvareNavn").val(response.commodity_Name);
+			$("#showRåvareId").val(response.commodity_id);
+
+			applyModal();
+
+		},
+		error: function (jqXHR, text, error) {
+			alert(jqXHR.status + text + error);
+		},
+	});
+}
+
+function updateCommodity() {
+	toggleModal();
+	console.log("update started");
+	const commodity = {
+		commodity_Name: $("#showRåvareNavn").val(),
+		commodity_id: $("#showRåvareId").val(),
+	};
+
+	console.log(commodity.commodity_Name);
+
+	$.ajax({
+		url: `https://api.mama.sh/commodity`,
+		contentType: "application/json",
+		method: "PUT",
+		data: JSON.stringify(commodity),
+		succes: function (response) {
+			listCommodities();
+		},
+		error: function (jqXHR, text, error) {
+			toggleModal();
+			alert(jqXHR.status + text + error);
+		},
+	});
+}
+
+function applyModal() {
 	var openmodal = document.querySelectorAll('.modal-open')
 	for (var i = 0; i < openmodal.length; i++) {
-		openmodal[i].addEventListener('click', function (event) {
+		openmodal[i].addEventListener('click', function (event, id) {
 			event.preventDefault()
 			toggleModal()
 		})
 	}
-
 	const overlay = document.querySelector('.modal-overlay')
 	overlay.addEventListener('click', toggleModal)
 
@@ -27,45 +215,25 @@ $(document).ready(() => {
 			toggleModal()
 		}
 	};
-});
-
-
-
-
-function toggleModal() {
-	const body = document.querySelector('body')
-	const modal = document.querySelector('.modal')
-	modal.classList.toggle('opacity-0')
-	modal.classList.toggle('pointer-events-none')
-	body.classList.toggle('modal-active')
 }
 
-function listCommodities() {
-	console.log("test");
-	// document.getElementById("loaderID").style.display = "block";
-	$.ajax({
-		url: "https://api.mama.sh/userresource",
-		contentType: "application/json",
-		method: "GET",
-		success: function (response) {
-			//document.getElementById("loaderID").style.display = "none";
-			$("#listOfCommoditiestable").html("");
-			var html = "";
-			//'<table class="tableOfUsers"> <tr><th>Name</th><th>Id</th></tr>';
-			jQuery.each(response, (i, item) => {
-				html += `<tr>`;
-				html += `<td> ${item.userID}  ${item.firstName} ${item.surname}</td>`;
-				html += `<td><button class="w3-dark-grey list-item-btn" onclick="inactiveUser(${item.userID});"> ${getStatus(item.status)} <i class="fa fa-close"></i></button> <button onclick="getUser(${item.userID});" id="EditBtn" class="w3-dark-grey list-item-btn">Vis <i class="fa fa-cog fa-fw"></i></button></td>`;
-				html += `</tr>`;
-			});
-			console.log(html);
-			$("#listOfCommoditiestable").append(html);
 
-			$("#listOfCommoditiestable").show();
-		},
-		error: function (jqXHR, text, error) {
-			document.getElementById("loaderID").style.display = "none";
-			alert(jqXHR.status + text + error);
-		},
-	})
-};
+/*
+if (style === 1) {
+
+		$("#modal-footer").append(
+			<button
+				class="modal-update px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
+				onclick="updateCommodity()">Opdater</button>
+			<button
+				class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
+		)
+
+	} elseif(style === 2) {
+		<button
+			class="modal-update px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
+			onclick="createCommodity()">Opret</button>
+			<button
+				class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
+	}
+*/
